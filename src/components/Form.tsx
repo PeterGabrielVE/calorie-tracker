9365c99110c788cf52e99384b8502850dc917bfc
Pedro Gabriel <gaboleal123@gmail.com>
2024-11-19 22:08:42 -0400

    category

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,26 @@
+import { categories } from "../data/categories";
 
 export default function Form() {
   return (
     <div>
-       <h1>form</h1>
+       <form className="space-y-5 bg-white shadow p-10 rounded-lg">
+        <div className="grid grid-cols-1 gap-3">
+          <label htmlFor="category" className="font-bold">Categoría:</label>
+          <select 
+            className="border border-slate-300 p-2 rounded-lg w-full bg-white"
+            name="" id="category">
+              {categories.map(category => (
+                <option
+                key={category.id}
+                value={category.id}
+                >{category.name} </option> 
+              ))
+
+              }
+
+            </select>
+        </div>
+       </form>
     </div>
   )
 }
