80dbadf63775f3b0e78a51d10097d7d2ada1bedc
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2024-12-12 11:47:22 -0400

    manipulando el state

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -9,7 +9,12 @@ export default function Form() {
     calories:0
   });
 
- 
+  const handleChange = (e) =>{
+    setActivity({
+      ...activity,
+      [e.target.id]: e.target.value
+    })
+  }
   return (
     <div>
        <form className="space-y-5 bg-white shadow p-10 rounded-lg">
@@ -17,7 +22,7 @@ export default function Form() {
           <label htmlFor="category" className="font-bold">Categoría:</label>
           <select 
             className="border border-slate-300 p-2 rounded-lg w-full bg-white"
-            name="" id="category" value={activity.category}>
+            name="" id="category" value={activity.category} onChange={handleChange}>
               {categories.map(category => (
                 <option
                 key={category.id}
@@ -27,12 +32,13 @@ export default function Form() {
             </select>
         </div>
         <div className="grid grid-cols-1 gap-3">
-          <label htmlFor="activity" className="font-bold">Actividad:</label>
-          <input id="activity"
+          <label htmlFor="name" className="font-bold">Actividad:</label>
+          <input id="name"
                   type="text"
                   className="border border-slate-300 p-2 rounded-lg"
                   placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicletas"
-                  value={activity.name} />
+                  value={activity.name} 
+                  onChange={handleChange}/>
         </div>
         <div className="grid grid-cols-1 gap-3">
           <label htmlFor="activity" className="font-bold">Calorías:</label>
@@ -40,7 +46,8 @@ export default function Form() {
                   type="number"
                   className="border border-slate-300 p-2 rounded-lg"
                   placeholder="Calorías. ej. 300 0 500"
-                  value={activity.calories} />
+                  value={activity.calories}
+                  onChange={handleChange} />
         </div>
         <input type="submit" className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
         value="Guardar comida o Guardar Ejercicio" />
