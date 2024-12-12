0a5b229a7d37b53ff2d97b82251bc40a5cda56d8
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2024-12-12 11:29:48 -0400

    creando el state

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
 import { categories } from "../data/categories";
 
 export default function Form() {
+
+  const [activity, setActivity] = useState({
+    category:'1',
+    name:'',
+    calories:0
+  });
+
+ 
   return (
     <div>
        <form className="space-y-5 bg-white shadow p-10 rounded-lg">
@@ -8,16 +17,13 @@ export default function Form() {
           <label htmlFor="category" className="font-bold">Categoría:</label>
           <select 
             className="border border-slate-300 p-2 rounded-lg w-full bg-white"
-            name="" id="category">
+            name="" id="category" value={activity.category}>
               {categories.map(category => (
                 <option
                 key={category.id}
                 value={category.id}
                 >{category.name} </option> 
-              ))
-
-              }
-
+              ))}
             </select>
         </div>
         <div className="grid grid-cols-1 gap-3">
@@ -25,14 +31,16 @@ export default function Form() {
           <input id="activity"
                   type="text"
                   className="border border-slate-300 p-2 rounded-lg"
-                  placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicletas" />
+                  placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicletas"
+                  value={activity.name} />
         </div>
         <div className="grid grid-cols-1 gap-3">
           <label htmlFor="activity" className="font-bold">Calorías:</label>
           <input id="calories"
                   type="number"
                   className="border border-slate-300 p-2 rounded-lg"
-                  placeholder="Calorías. ej. 300 0 500" />
+                  placeholder="Calorías. ej. 300 0 500"
+                  value={activity.calories} />
         </div>
         <input type="submit" className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
         value="Guardar comida o Guardar Ejercicio" />
