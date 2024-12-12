b25316da14bcb29214a9d048399a7d24ff98f6b3
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2024-12-12 11:16:36 -0400

    form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -20,6 +20,22 @@ export default function Form() {
 
             </select>
         </div>
+        <div className="grid grid-cols-1 gap-3">
+          <label htmlFor="activity" className="font-bold">Actividad:</label>
+          <input id="activity"
+                  type="text"
+                  className="border border-slate-300 p-2 rounded-lg"
+                  placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicletas" />
+        </div>
+        <div className="grid grid-cols-1 gap-3">
+          <label htmlFor="activity" className="font-bold">Calorías:</label>
+          <input id="calories"
+                  type="number"
+                  className="border border-slate-300 p-2 rounded-lg"
+                  placeholder="Calorías. ej. 300 0 500" />
+        </div>
+        <input type="submit" className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
+        value="Guardar comida o Guardar Ejercicio" />
        </form>
     </div>
   )
