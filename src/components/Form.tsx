51f0359c93b8c2193d239bcd48415f58aa5f03df
PeterGabrielVE <31576642+PeterGabrielVE@users.noreply.github.com>
2024-12-28 16:22:21 -0400

    mostrar text guardar

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -60,7 +60,7 @@ export default function Form() {
         </div>
         <input type="submit" 
                 className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer disabled:opacity-10"
-                value="Guardar comida o Guardar Ejercicio"
+                value={activity.category === 1 ? 'Guardar comida' : 'Guardar ejercicio'}
                 disabled={!isValidActivity()} />
        </form>
     </div>
