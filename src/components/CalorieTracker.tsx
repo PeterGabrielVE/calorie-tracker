402c6c2c18349f4d39e5c9451b29046c84889dcd
PeterGabrielVE <31576642+PeterGabrielVE@users.noreply.github.com>
2024-12-29 17:24:20 -0400

    componentes de calorias mostradas

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -1,5 +1,6 @@
 import { useMemo } from "react";
 import { Activity } from "../types";
+import CaloriesDisplay from "./CaloriesDisplay";
 
 type CalorieTrackerProps = {
     activities: Activity[];
@@ -7,14 +8,14 @@ type CalorieTrackerProps = {
 
 const CalorieTracker = ({ activities }: CalorieTrackerProps) => {
     const caloriesConsumed = useMemo(() => activities.reduce((total, activity) => activity.category === 1 ? total + activity.calories : total,0) ,[activities] );
+    const caloriesBurned = useMemo(() => activities.reduce((total, activity) => activity.category === 2 ? total + activity.calories : total,0) ,[activities] );
   return (
     <>
       <h2 className="text-4xl font-black text-white text-center">Resumen de Calorías</h2>
-      <div className="flex flex-col items-center md:flex row md:justify-between gap-5 mt-10">
-        <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
-            <span className="font-black text-6xl text-orange"> {caloriesConsumed}</span>
-           Consumidas
-        </p>
+      <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
+        <CaloriesDisplay calories={caloriesConsumed}  text="Consumidas" />
+        
+        <CaloriesDisplay calories={caloriesBurned}  text="Ejercicio" />
       </div>
      
     </>
