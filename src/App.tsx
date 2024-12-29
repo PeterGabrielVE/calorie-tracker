b29970535091a295844c8a3a056db1620f677c9e
PeterGabrielVE <31576642+PeterGabrielVE@users.noreply.github.com>
2024-12-29 17:15:02 -0400

    mostrando calorias consumida

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useReducer } from 'react';
 import Form from './components/Form';
 import { activityReducer, initialState } from './reducers/activity-reducer';
 import ActivityList from './components/ActivityList';
+import CalorieTracker from './components/CalorieTracker';
 function App() {
 
   const [state, dispatch] = useReducer(activityReducer, initialState);
@@ -33,6 +34,13 @@ function App() {
             state={state}
           />
         </div>
+      </section>
+      <section className='bg-gray-800 py-10'>
+         <div className='max-w-xl mx-auto'>
+          <CalorieTracker
+            activities={state.activities}
+            />
+         </div>
       </section>
       <section className='p-10 mx-auto max-w-4xl'>
         <ActivityList 
