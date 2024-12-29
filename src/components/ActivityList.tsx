08e0137e822add31853f4348649a45b22f5ea0e0
PeterGabrielVE <31576642+PeterGabrielVE@users.noreply.github.com>
2024-12-29 10:33:08 -0400

    validar contenido

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -12,11 +12,14 @@ const ActivityList = ({ activities, dispatch } : ActivityListProps) => {
  
     const categoryName = useMemo(() => (category:  Activity['category'])=> categories.map( cat => cat.id === category ? cat.name : '' ), [] )
 
+    const isEmptyActivities = useMemo(()  => activities.length === 0, [activities]);
   return (
     <>
         <h2 className='text-4xl font-bold text-slate-600 text-center'>Comida y Actividades</h2>
 
-        {activities.map( activity => (
+        {isEmptyActivities ? <p className="text-center my-4">No hay actividades aún...</p> :
+        
+        activities.map( activity => (
             <div key={activity.id} className="px-5 py-10 bg-white mt-5 flex justify-between">
                <div className="space-y-2 relative">
                     <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold ${ activity.category=== 1 ? 'bg-lime-500' : 'bg-orange-500' }`} >
